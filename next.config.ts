aaa4dd1e800a9da745c1d34ba3525475500080bd
Joe W. <joewade0110@gmail.com>
2025-12-31 12:49:53 -0500

    fix(config): add picsum.photos to allowed image domains

--- next.config.ts
+++ next.config.ts
@@ -47,6 +47,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'i.ytimg.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
     ],
   },
   async headers() {
